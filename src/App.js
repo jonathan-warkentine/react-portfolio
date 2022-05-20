f2b446dd8826aa201c849601a43c42fa0c512b9b
Jonathan Warkentine <jonathan.warkentine@gmail.com>
2022-05-20 18:24:01 -0400

    React routing, file structure in place

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {Home, About, Projects} from './pages';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="flex-column justify-center align-center min-100-vh bg-primary">
+        <Routes>
+          <Route 
+            path="/" 
+            element={<Home />} 
+          />
+          <Route 
+            path="/about" 
+            element={<About />} 
+          />
+          <Route 
+            path="/projects" 
+            element={<Projects />} 
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
